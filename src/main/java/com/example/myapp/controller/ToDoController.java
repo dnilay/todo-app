81bffb56fead88990d26ee46f4d19b8d0a90ebe9
nilay <mailnilaydas@gmail.com>
2021-12-02 11:41:39 +0530

    2nd commit

--- src/main/java/com/example/myapp/controller/ToDoController.java
+++ src/main/java/com/example/myapp/controller/ToDoController.java
@@ -3,13 +3,11 @@ package com.example.myapp.controller;
 import com.example.myapp.dao.ToDoDao;
 import com.example.myapp.model.ToDo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
+@CrossOrigin(origins = "*")
 public class ToDoController {
 
     private ToDoDao toDoDao;
